590a4d6166214cf68c6d68f27e16ce308b628082
maru <masarushimoji@hotmail.com>
2024-10-17 13:26:34 +0100

    Started on code that finds the file that has been changed

--- client.py
+++ client.py
@@ -6,6 +6,8 @@ import time
 import socket
 
 prevListLength = -1
+prevList= []
+prevListHashes = []
 
 
 #HASHING FUNCTION
@@ -18,7 +20,7 @@ def hashDir (dir):
 
        # for file in files:
         #    print(file)   
-            
+
         for file in files:
             if os.path.isfile(file):
                 with open(file, 'rb') as f:
@@ -40,25 +42,59 @@ if len(sys.argv) > 1:
         while not(fileChanged):
             currentHash = hashDir(dir)
             if (hash == -1):
-                hash = currentHash
-                prevList = len(glob.glob(os.path.join(dir, '*')))
+                hash = currentHash 
             elif (hash != currentHash):
-                hash == currentHash
+                hash = currentHash
                 print("Hash Changed")
                 fileChanged = True
-                if (len(glob.glob(os.path.join(dir, '*'))) > prevListLength):
+                currentList = len(glob.glob(os.path.join(dir, '*'))) 
+                if (currentList > prevListLength):
                     print("file added")
-                elif (len(glob.glob(os.path.join(dir, '*'))) < prevListLength):
+                    actionNeeded = "add"
+                elif (currentList < prevListLength):
                     print("file removed")
+                    actionNeeded = "remove"
                 else:
                     print("file changed")
+                    actionNeeded = "replace"
             else:
                 print("Files unchanged.")
+            
+            prevList = glob.glob(os.path.join(dir, '*'))
             prevListLength = len(glob.glob(os.path.join(dir, '*')))
+
+            hashFunc = hashlib.new('sha256')
+            prevListHashes = []
+            for file in prevList:
+                if os.path.isfile(file):
+                    with open(file, 'rb') as f:
+                        while chunk := f.read(8192):
+                            hashFunc.update(chunk)
+                        prevListHashes.append(hashFunc.hexdigest())
+
+
             time.sleep(1)
 
 
-        
+        print("action needed: " + actionNeeded)
+        currentHash = []
+        currentList = glob.glob(os.path.join(dir, '*')) 
+
+        if (actionNeeded == "replace"):
+            hashFunc = hashlib.new('sha256')
+            for file in currentList:
+                if os.path.isfile(file):
+                    with open(file, 'rb') as f:
+                        while chunk := f.read(8192):
+                            hashFunc.update(chunk)
+                        currentHash.append(hashFunc.hexdigest())
+                
+            
+        print("prev file hashes:" + str(prevListHashes))
+        print("current file hashes: " + str(currentHash))
+
+
+
         host = socket.gethostname()
         port = 5000
 
